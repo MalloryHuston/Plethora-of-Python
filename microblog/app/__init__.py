e79f119f8e8b3070ca2aee9cd06fc02385f3eed3
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-09-09 07:24:29 -0700

    Chapter 7: Error Handling

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
- 
 
 app = Flask(__name__)
 app.config.from_object(Config)
