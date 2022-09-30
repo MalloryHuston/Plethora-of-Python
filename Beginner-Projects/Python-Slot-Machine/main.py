74b0711448440ce079564a5f61a12fabd5b218a9
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-09-30 16:30:20 -0700

    Update main.py

--- Beginner-Projects/Python-Slot-Machine/main.py
+++ Beginner-Projects/Python-Slot-Machine/main.py
@@ -1,1 +1,39 @@
 import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+symbol_value = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+      
+        
