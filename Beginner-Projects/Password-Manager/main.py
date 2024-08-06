d2296853a01352960d512e2c69ec443906abb0b4
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2024-08-06 06:56:43 -0700

    Updated main.py

--- Beginner-Projects/Password-Manager/main.py
+++ Beginner-Projects/Password-Manager/main.py
@@ -1,21 +1,24 @@
-from cryptography.fernet import Fernet # type: ignore
+import os
+from cryptography.fernet import Fernet  # type: ignore
+
+# Define the base directory (the directory where this script is located)
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 '''
 # Generate a key and save it in a file
 def write_key():
     key = Fernet.generate_key()
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/password-manager/key.key', "wb") as key_file:
+    key_path = os.path.join(base_dir, 'key.key')
+    with open(key_path, "wb") as key_file:
         key_file.write(key)
-
-write_key()
 '''
 
 
 def load_key():
-    file = open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/password-manager/key.key', "rb")
-    key = file.read()
-    file.close()
+    key_path = os.path.join(base_dir, 'key.key')
+    with open(key_path, "rb") as file:
+        key = file.read()
     return key
 
 key = load_key()
@@ -23,19 +26,20 @@ fer = Fernet(key)
 
 
 def view():
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/password-manager/passwords.txt', 'r') as f:
+    password_path = os.path.join(base_dir, 'passwords.txt')
+    with open(password_path, 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "| Password:",
-                  fer.decrypt(passw.encode()).decode())
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
 
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/password-manager/passwords.txt', 'a') as f:
+    password_path = os.path.join(base_dir, 'passwords.txt')
+    with open(password_path, 'a') as f:
         f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
