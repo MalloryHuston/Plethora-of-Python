baf7fef03d3ecde8787a2711191b4e8cc4018a09
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2024-08-06 06:52:24 -0700

    Added files via upload

---
+++ Beginner-Projects/Python-Beginner-Automation-Projects/backup.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+##### Macintosh #####
+source_dir = "/Users/malpal101/Pictures/Snagit/Autosaved Captures"
+destination_dir = "/Users/malpal101/Desktop/Backups"
+
+##### Windows #####
+# source_dir = "C:\Users\User\Pictures\Screenshots"
+# destination_dir = "C:\Users\User\Desktop\Backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+    
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+
+schedule.every().day.at("18:57").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
---
+++ Beginner-Projects/Python-Beginner-Automation-Projects/currency.py
@@ -0,0 +1,31 @@
+import requests
+
+API_KEY = 'fca_live_usFNMnbUOTwqIK8Oiw4fAI5qbmxDH7EXVAuwP37v'
+BASE_URL = f"htt[s://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
+
+CURRENCIES = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CNY", "KRW", "INR"]
+
+def convert_currency(base):
+    currencies = ",".join(CURRENCIES)
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data["data"]
+    except:
+        print("Invalid currency.")
+        return None
+    
+while True:
+    base = input("Enter the base currency (q for quit): ").upper()
+
+    if base == "Q":
+        break
+
+    data = convert_currency(base)
+    if not data:
+        continue
+
+    del data[base]
+    for ticker, value in data.items():
+        print(f"{ticker}: {value}")
