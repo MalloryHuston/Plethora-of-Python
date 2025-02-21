cb953590ff4ebd55071a400926b6f1627280ce4b
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2025-02-21 03:28:21 -0800

    Updated ability_score_generator.py

--- Beginner-Projects/Python-Projects-for-Noobs/D&D 5e/dnd_ability_score_generator.py
+++ Beginner-Projects/Python-Projects-for-Noobs/D&D 5e/dnd_ability_score_generator.py
@@ -3,19 +3,23 @@ import random
 
 def roll_4d6_drop_lowest():
     """Roll 4 six-sided dice and drop the lowest one."""
-    rolls = [random.randint(1, 6) for _ in range(4)]    # Roll four 6-sided dice
+    # Roll four 6-sided dice
+    rolls = [random.randint(1, 6) for _ in range(4)]
     rolls.sort()    # Sort the rolls to easily drop the lowest
     return sum(rolls[1:])   # Sum the top 3 dice (drop the lowest one)
 
 
 def generate_ability_scores():
-    """Generate 6 ability scores by rolling 4d6 and dropping the lowest roll."""
-    ability_scores = [roll_4d6_drop_lowest() for _ in range(6)] # Roll 6 times
+    """Generate 6 ability scores by rolling 4d6
+    and dropping the lowest roll."""
+    # Roll 6 times
+    ability_scores = [roll_4d6_drop_lowest() for _ in range(6)]
     return ability_scores
 
 
 def main():
-    """Main function to run the script and ask if the user wants to reroll their ability scores."""
+    """Main function to run the script and ask if the user
+    wants to reroll their ability scores."""
     while True:
         # Generate aned display ability scores
         scores = generate_ability_scores()
@@ -23,7 +27,8 @@ def main():
 
         # Ask the user if they want to generate another set of scores
         while True:
-            rerun = input("Would you like to roll again? (y/n): ").strip().lower()
+            rerun = input
+            ("Would you like to roll again? (y/n): ").strip().lower()
 
             if rerun == 'y':
                 break   # Valid input, rerun the loop
