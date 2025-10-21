4a92961edd612dc1b0892990be57509e34f4280d
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-10-21 06:00:40 -0700

    Add files via upload

---
+++ Web-Scraping/All_Links_from_Given_Webpage/README.md
@@ -0,0 +1,23 @@
+# All Links from Given Webpage
+
+This script retrieves all links from a given webpage and saves them as a ``.txt`` file.
+
+### Prerequisites
+
+Install the following required modules:
+
+``` bash
+pip install beautifulsoup4 requests
+```
+
+### How to Run the Script
+
+``` bash
+python get_links.py
+```
+
+You will then be asked which webpage you would like to analyze.
+After that, the extracted links will be saved as an array in the file ``myLinks.txt``.
+
+## *Authors' Names*
+[Mitesh](https://github.com/Mitesh2499) & [Michael Mba](https://github.com/mikeysan)
---
+++ Web-Scraping/All_Links_from_Given_Webpage/get_links.py
@@ -0,0 +1,18 @@
+import requests as rq
+from bs4 import BeautifulSoup
+
+url = input("Enter link: ")
+if ("https" or "http") in url:
+    data = rq.get(url)
+else:
+    data = rq.get("https://" + url)
+soup = BeautifulSoup(data.text, "html.parser")
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+
+# Writing the output to a file (myLinks.txt) instead of to stdout
+# You can change 'a' to 'w' to overwrite the file each time
+with open("myLinks.txt", 'a') as saved:
+    print(links[:10], file=saved)
+print(f"Total {len(links)} links found. Check myLinks.txt for the output.")
