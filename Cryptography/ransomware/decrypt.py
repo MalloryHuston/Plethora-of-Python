9e5563f0024597b4784f7d51c1d8c12db4eaf163
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-10-21 13:37:47 -0700

    Update decrypt.py

--- Cryptography/ransomware/decrypt.py
+++ Cryptography/ransomware/decrypt.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-
 import os
 from cryptography.fernet import Fernet
 
-# let's find some files
-
+# Let's find some files!
 files = []
 
 for file in os.listdir():
-	if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file)
+    if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
 print(files)
 
 with open("thekey.key", "rb") as key:
-	secretkey = key.read()
+    secret_key = key.read()
 
-secretphrase = "coffee"
+secret_phrase = "coffee"
 
-user_phrase = input("Enter the secret phase to decrypt your files:\n ")
+user_phrase = input("Enter the secret phrase to decrypt your files:\n ")
 
-if user_phrase == secretphrase:
-	for file in files:
-		with open(file, "rb") as thefile:
-			contents = thefile.read()
-		contents_decrypted = Fernet(secretkey).decrypt(contents)
-		with open(file, "wb") as thefile:
-			thefile.write(contents_decrypted)
-		print("Congrats, your files have been decrypted. Enjoy your coffee.")
+if user_phrase == secret_phrase:
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_decrypted = Fernet(secret_key).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
+        print("Congrats, your files have been decrypted. Enjoy your coffee.")
 else:
-	print("Sorry, wrong secret phrase. Send me more Bitcoin.")
+    print("Sorry, wrong secret phrase. Send me more Bitcoin.")
