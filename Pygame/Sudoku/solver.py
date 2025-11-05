8cf6542e21b44d2707b166bbe98dd4678cbe2754
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-05 07:56:13 -0800

    Update solver.py

--- Pygame/Sudoku/solver.py
+++ Pygame/Sudoku/solver.py
@@ -7,7 +7,7 @@ def solve(bo):
     else:
         row, col = find
 
-    for i in range(1,10):
+    for i in range(1, 10):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
 
@@ -36,7 +36,7 @@ def valid(bo, num, pos):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if bo[i][j] == num and (i, j) != pos:
                 return False
 
     return True
