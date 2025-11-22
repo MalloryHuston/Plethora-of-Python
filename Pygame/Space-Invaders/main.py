6209e636b6cd10f596c303541af7797ff63d55a2
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-22 14:26:32 -0800

    Update main.py

--- Pygame/Space-Invaders/main.py
+++ Pygame/Space-Invaders/main.py
@@ -13,3 +13,15 @@ IMG_DIR = "/Users/malpal101/Plethora-of-Python/Pygame/Space-Invaders/imgs"
 RED_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_red_small.png"))
 GREEN__SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_blue_small.png"))
+
+# Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_yellow.png"))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_yellow.png"))
+
+# Background
+BG = pygame.image.load(os.path.join(IMG_DIR, "background-black.png"))
