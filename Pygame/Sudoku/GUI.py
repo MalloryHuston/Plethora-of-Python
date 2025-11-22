ca840b54b73b468e59269366df9f71fe6586c0de
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-22 12:42:23 -0800

    Update GUI.py

--- Pygame/Sudoku/GUI.py
+++ Pygame/Sudoku/GUI.py
@@ -1,6 +1,7 @@
 # GUI.py
 import pygame
 import time
+
 pygame.font.init()
 
 
@@ -14,13 +15,16 @@ class Grid:
         [9, 0, 4, 0, 6, 0, 0, 0, 5],
         [0, 7, 0, 3, 0, 0, 0, 1, 2],
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+        [0, 4, 9, 2, 0, 6, 0, 0, 7],
     ]
 
     def __init__(self, rows, cols, width, height, win):
         self.rows = rows
         self.cols = cols
-        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.cubes = [
+            [Cube(self.board[i][j], i, j, width, height) for j in range(cols)]
+            for i in range(rows)
+        ]
         self.width = width
         self.height = height
         self.model = None
@@ -29,7 +33,10 @@ class Grid:
         self.win = win
 
     def update_model(self):
-        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+        self.model = [
+            [self.cubes[i][j].value for j in range(self.cols)]
+            for i in range(self.rows)
+        ]
 
     def place(self, val):
         row, col = self.selected
@@ -52,13 +59,21 @@ class Grid:
     def draw(self):
         # Draw Grid Lines
         gap = self.width / 9
-        for i in range(self.rows+1):
+        for i in range(self.rows + 1):
             if i % 3 == 0 and i != 0:
                 thick = 4
             else:
                 thick = 1
-            pygame.draw.line(self.win, (0, 0, 0), (0, i*gap), (self.width, i*gap), thick)
-            pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+            pygame.draw.line(
+                self.win, (0, 0, 0), (0, i * gap), (self.width, i * gap), thick
+            )
+            pygame.draw.line(
+                self.win,
+                (0, 0, 0),
+                (i * gap, 0),
+                (i * gap, self.height),
+                thick,
+            )
 
         # Draw Cubes
         for i in range(self.rows):
@@ -169,10 +184,16 @@ class Cube:
 
         if self.temp != 0 and self.value == 0:
             text = fnt.render(str(self.temp), 1, (128, 128, 128))
-            win.blit(text, (x+5, y+5))
+            win.blit(text, (x + 5, y + 5))
         elif not (self.value == 0):
             text = fnt.render(str(self.value), 1, (0, 0, 0))
-            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+            win.blit(
+                text,
+                (
+                    x + (gap / 2 - text.get_width() / 2),
+                    y + (gap / 2 - text.get_height() / 2),
+                ),
+            )
 
         if self.selected:
             pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
@@ -187,7 +208,13 @@ class Cube:
         pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
 
         text = fnt.render(str(self.value), 1, (0, 0, 0))
-        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        win.blit(
+            text,
+            (
+                x + (gap / 2 - text.get_width() / 2),
+                y + (gap / 2 - text.get_height() / 2),
+            ),
+        )
         if g:
             pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
         else:
@@ -224,8 +251,8 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
             if bo[i][j] == num and (i, j) != pos:
                 return False
 
@@ -247,8 +274,8 @@ def redraw_window(win, board, time, strikes):
 
 def format_time(secs):
     sec = secs % 60
-    minute = secs//60
-    hour = minute//60
+    minute = secs // 60
+    hour = minute // 60
 
     mat = " " + str(minute) + ":" + str(sec)
     return mat
