822f5eaa67a23f60cf2836fbc04e912e143c280e
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-22 14:20:55 -0800

    Update main.py

--- Pygame/Space-Invaders/main.py
+++ Pygame/Space-Invaders/main.py
@@ -9,5 +9,7 @@ IMG_DIR = "/Users/malpal101/Plethora-of-Python/Pygame/Space-Invaders/imgs"
 # Load images (Windows)
 # IMG_DIR = "C:/Users/User/Plethora-of-Python/Pygame/Space-Invaders/imgs"
 
-RED_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR,
-                                                "pixel_ship_red_small.png"))
+# Enemy ships
+RED_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_red_small.png"))
+GREEN__SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join(IMG_DIR, "pixel_ship_blue_small.png"))
