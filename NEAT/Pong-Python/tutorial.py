7945644e455879e9d72eef40c7b32ce17f4a5a4f
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-22 12:11:58 -0800

    Update tutorial.py

--- NEAT/Pong-Python/tutorial.py
+++ NEAT/Pong-Python/tutorial.py
@@ -31,7 +31,13 @@ class PongGame:
                 self.game.move_paddle(left=True, up=False)
 
             output = net.activate(
-                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+                (
+                    self.right_paddle.y,
+                    self.ball.y,
+                    abs(self.right_paddle.x - self.ball.x),
+                )
+            )
+
             decision = output.index(max(output))
 
             if decision == 0:
@@ -58,7 +64,12 @@ class PongGame:
                     quit()
 
             output1 = net1.activate(
-                (self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))
+                (
+                    self.left_paddle.y,
+                    self.ball.y,
+                    abs(self.left_paddle.x - self.ball.x),
+                )
+            )
             decision1 = output1.index(max(output1))
 
             if decision1 == 0:
@@ -69,7 +80,13 @@ class PongGame:
                 self.game.move_paddle(left=True, up=False)
 
             output2 = net2.activate(
-                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+                (
+                    self.right_paddle.y,
+                    self.ball.y,
+                    abs(self.right_paddle.x - self.ball.x),
+                )
+            )
+
             decision2 = output2.index(max(output2))
 
             if decision2 == 0:
@@ -84,7 +101,11 @@ class PongGame:
             game.draw(draw_score=False, draw_hits=True)
             pygame.display.update()
 
-            if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_hits > 50:
+            if (
+                game_info.left_score >= 1
+                or game_info.right_score >= 1
+                or game_info.left_hits > 50
+            ):
                 self.calculate_fitness(genome1, genome2, game_info)
                 break
 
@@ -101,15 +122,15 @@ def eval_genomes(genomes, config):
         if i == len(genomes) - 1:
             break
         genome1.fitness = 0
-        for genome_id2, genome2 in genomes[i+1:]:
-            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+        for genome_id2, genome2 in genomes[i + 1:]:
+            genome2.fitness = 0 if genome2.fitness is None else genome2.fitness
             game = PongGame(window, width, height)
             game.train_ai(genome1, genome2, config)
 
 
 def run_neat(config):
-    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-7')
-    #p = neat.Population(config)
+    p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-7")
+    # p = neat.Population(config)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
@@ -135,8 +156,12 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
 
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_path)
+    config = neat.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path,
+    )
     # run_neat(config)
     test_ai(config)
