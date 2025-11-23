73ab17a280016466ce9e177fb5bdf944ce5a3b30
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-22 20:48:51 -0800

    Update main.py

--- Pygame/Space-Invaders/main.py
+++ Pygame/Space-Invaders/main.py
@@ -12,39 +12,40 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
-# Load images (macOS)
-IMG_DIR = "/Users/malpal101/Plethora-of-Python/Pygame/Space-Invaders/assets"
-
-# Load images (Windows)
-# IMG_DIR = "C:/Users/User/Plethora-of-Python/Pygame/Space-Invaders/assets"
-
+# Load images
 # Enemy ships
 RED_SPACE_SHIP = pygame.image.load(
-    os.path.join(IMG_DIR, "pixel_ship_red_small.png")
+    os.path.join("assets/imgs", "pixel_ship_red_small.png")
 )
 GREEN_SPACE_SHIP = pygame.image.load(
-    os.path.join(IMG_DIR, "pixel_ship_green_small.png")
+    os.path.join("assets/imgs", "pixel_ship_green_small.png")
 )
 BLUE_SPACE_SHIP = pygame.image.load(
-    os.path.join(IMG_DIR, "pixel_ship_blue_small.png")
+    os.path.join("assets/imgs", "pixel_ship_blue_small.png")
 )
 
 # Player ship
 YELLOW_SPACE_SHIP = pygame.image.load(
-    os.path.join(IMG_DIR, "pixel_ship_yellow.png")
+    os.path.join("assets/imgs", "pixel_ship_yellow.png")
 )
 
-# Laser
-RED_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join(IMG_DIR, "pixel_laser_blue.png"))
+# Lasers
+RED_LASER = pygame.image.load(
+    os.path.join("assets/imgs", "pixel_laser_red.png")
+)
+GREEN_LASER = pygame.image.load(
+    os.path.join("assets/imgs", "pixel_laser_green.png")
+)
+BLUE_LASER = pygame.image.load(
+    os.path.join("assets/imgs", "pixel_laser_blue.png")
+)
 YELLOW_LASER = pygame.image.load(
-    os.path.join(IMG_DIR, "pixel_laser_yellow.png")
+    os.path.join("assets/imgs", "pixel_laser_yellow.png")
 )
 
 # Background
 BG = pygame.transform.scale(
-    pygame.image.load(os.path.join(IMG_DIR, "background-black.png")),
+    pygame.image.load(os.path.join("assets/imgs", "background-black.png")),
     (WIDTH, HEIGHT),
 )
 
