e4c78a360b5cb7fb227ff434fc582621fcf821cf
mhuston <55804897+MalloryHuston@users.noreply.github.com>
2025-11-25 12:24:19 -0800

    Update main.py

--- Beginner-Projects/5-Python-Projects-for-Beginners/Password-Manager/main.py
+++ Beginner-Projects/5-Python-Projects-for-Beginners/Password-Manager/main.py
@@ -1,60 +1,66 @@
-from cryptography.fernet import Fernet # type: ignore
+from cryptography.fernet import Fernet  # type: ignore
 
 
-##### Macintosh #####
-#'/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/key.key'
+# MACINTOSH #
+# '/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/key.key'
 
-##### Windows #####
-#'C:\Users\User\Plethora-of-Python\Beginner-Projects\5-Python-Projects-For-Beginners\Password-Manager\key.key'
+# WINDOWS #
+# 'C:\Users\User\Plethora-of-Python\Beginner-Projects\5-Python-Projects-For-Beginners\Password-Manager\key.key'
 
-'''
+"""
 # Generate a key and save it in a file
 def write_key():
     key = Fernet.generate_key()
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/key.key', "wb") as key_file:
+    with open('key.key', "wb") as key_file:
         key_file.write(key)
 
 write_key()
-'''
+"""
 
 
 def load_key():
-    file = open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/key.key', "rb")
+    file = open("key.key", "rb")
     key = file.read()
     file.close()
     return key
+
 
 key = load_key()
 fer = Fernet(key)
 
 
-##### Macintosh #####
-#'/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/passwords.txt'
+# MACINTOSH #
+# '/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/passwords.txt'
 
-##### Windows #####
-#'C:\Users\User\Plethora-of-Python\Beginner-Projects\5-Python-Projects-For-Beginners\Password-Manager\passwords.txt'
+# WINDOWS #
+# 'C:\Users\User\Plethora-of-Python\Beginner-Projects\5-Python-Projects-For-Beginners\Password-Manager\passwords.txt'
 
 
 def view():
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/passwords.txt', 'r') as f:
+    with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "| Password:",
-                  fer.decrypt(passw.encode()).decode())
+            print(
+                "User:",
+                user,
+                "| Password:",
+                fer.decrypt(passw.encode()).decode(),
+            )
 
 
 def add():
-    name = input('Account Name: ')
+    name = input("Account Name: ")
     pwd = input("Password: ")
 
-    with open('/Users/malpal101/Plethora-of-Python/Beginner-Projects/5-Python-Projects-For-Beginners/Password-Manager/passwords.txt', 'a') as f:
+    with open("passwords.txt", "a") as f:
         f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
     mode = input(
-        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? "
+    ).lower()
     if mode == "q":
         break
     if mode == "view":
